454d87c736c2a030142d60ca4c54e7fefe63b971
kami <et284vu065k3@gmail.com>
2025-03-01 16:04:38 +0800

    update

--- lib/invoice_master.rb
+++ lib/invoice_master.rb
@@ -4,6 +4,9 @@ require_relative 'invoice_master/parser'
 require_relative 'invoice_master/llm_client'
 require_relative 'invoice_master/json_schema'
 require_relative 'invoice_master/sheet_export'
+require_relative 'invoice_master/ocr_client'
+require_relative 'invoice_master/claude_client'
+require_relative 'invoice_master/image_processor'
 
 module InvoiceMaster
   VERSION = '0.1.0'
